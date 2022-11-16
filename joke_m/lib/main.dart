1120a2896627376f408db063d28941ce0fe210bf
airwick989 <73313597+airwick989@users.noreply.github.com>
2022-11-16 13:00:49 -0500

    Finished most of the splash screen

--- joke_m/lib/main.dart
+++ joke_m/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color secondaryColor = Colors.indigoAccent;
 
@@ -22,8 +23,70 @@ class MyApp extends StatelessWidget {
             ),
             Image.asset(
               'images/logo.png',
-              width: 150,
-              height: 150,
+              width: 300,
+              height: 300,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Powered by Flutter',
+                  style: GoogleFonts.montserrat(
+                    textStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    )
+                  ),
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                const FlutterLogo(
+                  size: 30,
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'and Firebase',
+                  style: GoogleFonts.montserrat(
+                      textStyle: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      )
+                  ),
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Image.asset(
+                  'images/firebaseLogo.png',
+                  width: 30,
+                  height: 30,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Created by Group 23',
+                  style: GoogleFonts.montserrat(
+                      textStyle: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      )
+                  ),
+                ),
+              ],
             ),
           ],
         ),
